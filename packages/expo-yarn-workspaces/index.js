76dd75f061a6fb25bb50d13101bdf6def570cdeb
Erick Moreno <45931734+erickjmoreno@users.noreply.github.com>
2021-03-28 21:22:09 -0600

    Update index.js

--- packages/expo-yarn-workspaces/index.js
+++ packages/expo-yarn-workspaces/index.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const { getDefaultConfig } = require('@expo/metro-config');
 // TODO: Use the vendored metro config in a future version after SDK 41 is released
 // const { getDefaultConfig } = require('expo/metro-config');
-const blacklist = require('metro-config/src/defaults/blacklist');
+const blacklist = require('metro-config/src/defaults/exclusionList');
 const { assetExts } = require('metro-config/src/defaults/defaults');
 const path = require('path');
 
